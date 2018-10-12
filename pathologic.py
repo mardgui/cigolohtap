39198b1598b0cbc0b3d16beabbe9afcef32213fc
Quentin Guimard <quentin.guimard@etu.unice.fr>
2018-10-12 13:12:32 +0200

    Comments and documentation added

--- pathologic.py
+++ pathologic.py
@@ -1,5 +1,5 @@
 # -*-coding: utf-8 -*
-'''NAMES OF THE AUTHOR(S): Gaël Aglin <gael.aglin@uclouvain.be>, Francois Aubry <francois.aubry@uclouvain.be>'''
+'''NAMES OF THE AUTHOR(S): Gaël Aglin <gael.aglin@uclouvain.be>, Francois Aubry <francois.aubry@uclouvain.be>, Simon Calbert <simon.calbert@student.uclouvain.be>, Quentin Guimard <quentin.guimard@student.uclouvain.be>'''
 from search import *
 
 
@@ -7,19 +7,25 @@ from search import *
 # Problem class #
 #################
 class Pathologic(Problem):
+    """An extension of the Problem class, suited for the Pathologic problem."""
     
     def __init__(self, initial, goal=None):
-        self.initial = initial
-        self.goal = goal
+        """Constructor of the Pathologic class, with the same parameters as the Problem class."""
+        self.initial = initial # Initial state of the problem
+        self.goal = goal # Never used
+
+        # Fully scans the grid only once at the beggining to get the position of the player and the circles
         self.initial.set_init_pos()
         self.initial.set_init_circles()
+        
         self.nbNodesExplored = 0
 
     def successor(self, state):
+        """Successor method of the Pathologic class, with the same parameters as the Problem class."""
         self.nbNodesExplored = self.nbNodesExplored + 1
         i,j = state.pos
         
-        ## 
+        # Check for neighbor circles
         neigbors_circles = []
         if j+1 < state.nbc and state.grid[i][j+1] == '_':
             neigbors_circles.append([i,j+1]) 
@@ -32,35 +38,35 @@ class Pathologic(Problem):
             
         if i-1 >= 0 and state.grid[i-1][j] == '_':
             neigbors_circles.append([i-1,j]) 
-            
+        
+        # Check for deadlocks next to neighbor circles    
         for k in range(0,len(neigbors_circles)):
             pos = neigbors_circles[k]
             x = pos[0]
             y = pos[1]
             nb_access = 0
             if y+1 < state.nbc and state.grid[x][y+1] in ['0','_']:
-                nb_access = nb_access + 1
+                nb_access += 1
             if y-1 >= 0 and state.grid[x][y-1] in ['0','_']:
-                nb_access = nb_access + 1
+                nb_access += 1
             if x+1 < state.nbr and state.grid[x+1][y] in ['0','_']:
-                nb_access = nb_access + 1  
-            if x-1 >=0 and state.grid[x-1][y] in  ['0','_']:
-                nb_access = nb_access + 1  
+                nb_access += nb_access + 1  
+            if x-1 >=0 and state.grid[x-1][y] in ['0','_']:
+                nb_access += nb_access + 1  
             if nb_access == 0 and state.nb_circles > 1: 
                 return
                 yield
 
 
-
-        ## check if the map is split into 2 parts and so if the problem is stil solvable
-        # check for an horizontal split
+        ## Check if the map is split into 2 parts and so if the problem is stil solvable
+        # Check for an horizontal split
         horizontal = True
         for k in range(0,state.nbc):
             if k != j and state.grid[i][k] in ['0','_']:
                 if (i-1 >= 0 and state.grid[i-1][k] in ['0','_']) or (i+1 < state.nbr and state.grid[i+1][k] in ['0','_']):
                     horizontal = False
                     break
-        # check if circles in the bottom and the up part
+        # Check if there are circles in the top and in the bottom part
         if horizontal == True:
             up = False
             down = False
@@ -74,14 +80,14 @@ class Pathologic(Problem):
                    return
                    yield 
                    
-        # check for a vertical split
+        # Check for a vertical split
         vertical = True
         for k in range(0,state.nbr):
             if k != i and state.grid[k][j] in ['0','_']:
                 if (j-1 >= 0 and state.grid[k][j-1] in ['0','_']) or (j+1 < state.nbc and state.grid[k][j+1] in ['0','_']):
                     vertical = False
                     break
-        # check if circles in the left and in the right part
+        # Check if there are circles in the left and in the right part
         if vertical == True:
             left = False
             right = False
@@ -93,11 +99,10 @@ class Pathologic(Problem):
                     right = True
                 if left == True and right == True:
                    return
-                   yield 
+                   yield
             
             
-            
-        ## look wich actions is possible
+        ## Look which actions are possible
         if j+1 < state.nbc and state.grid[i][j+1] in ['0','_']:
             newState = state.clone()
             newState.move('right')
@@ -119,6 +124,8 @@ class Pathologic(Problem):
             yield ('up',newState)
         
     def goal_test(self, state):
+        """Goal_test method of the Pathologic class, with the same parameters as the Problem class."""
+        # This method has been extremely simplified thanks to a counter of the number of circles left in the grid
         return state.nb_circles == 0
 
 ###############
@@ -126,15 +133,19 @@ class Pathologic(Problem):
 ###############
 
 class State:
+    """The state class, containing the grid of the game."""
+
     def __init__(self, grid):
-        self.nbr = len(grid) # nb of row
-        self.nbc = len(grid[0])
+        """Constructor of the State class, with the grid as the only parameter."""
+        self.nbr = len(grid) # Number of rows
+        self.nbc = len(grid[0]) # Number of columns
         self.grid = grid
-        self.pos = None
-        self.nb_circles = 0
-        self.circles = []
+        self.pos = None # Position of the player
+        self.nb_circles = 0 # Number of circles left in the grid
+        self.circles = [] # Positions of the circles in the grid
 
     def __str__(self):
+        """Method used for displaying the grid, automatically called whenever print(State) is used."""
         s = ""
         for i in range(0, self.nbr):
             for j in range(0, self.nbc):
@@ -145,6 +156,7 @@ class State:
         return s
     
     def clone(self):
+        """Clone method of State, allowing a deep copy of the state class."""
         new_grid = [[0 for i in range(self.nbc)] for j in range(self.nbr)]
         for i in range (0,self.nbr):
             for j in range(0,self.nbc):
@@ -156,6 +168,7 @@ class State:
         return new_state
 
     def set_init_pos(self):
+        """Set_init_pos scans the grid and stores the position of the player in self.pos."""
         is_looping = True
         for i in range(0,self.nbr):
             for j in range(0,self.nbc):
@@ -167,6 +180,7 @@ class State:
                 break
             
     def set_init_circles(self):
+        """Set_init_pos scans the grid and stores the number of circles in self.nb_circles , as well as their positions in self.circles."""
         for i in range(0,self.nbr):
             for j in range(0,self.nbc):
                 if self.grid[i][j]=='_':
@@ -174,6 +188,8 @@ class State:
                     self.circles.append([i,j])
                 
     def move(self, direction):
+        """Move modifies the grid according to the chosen direction. The player is moved and is replaced by a cross.
+           The variables self.pos, self.nb_circles and self.nb_circles are updated if needed."""
         x,y = self.pos
         self.grid[x][y] = 'x'
         if direction == 'left':
